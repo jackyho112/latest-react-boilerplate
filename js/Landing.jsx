d3b9d67ebb65cf26fb0897dd3f791151a5dd89b8
jackyho112 <jackyho112dev@gmail.com>
2017-08-12 22:31:26 -0700

    Strip down

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,14 +2,14 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import type { RouterHistory } from 'react-router-dom';
 import { setSearchTerm } from './actionCreators';
 
 class Landing extends Component {
   props: {
-    searchTerm: string,
-    handleSearchTermChange: Function,
+    // searchTerm: string,
+    // handleSearchTermChange: Function,
     history: RouterHistory
   };
   goToSearch = (event: SyntheticEvent) => {
@@ -19,7 +19,8 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <h1>svideo</h1>
+        Hello World
+        {/* <h1>svideo</h1>
         <form onSubmit={this.goToSearch}>
           <input
             onChange={this.props.handleSearchTermChange}
@@ -28,7 +29,7 @@ class Landing extends Component {
             placeholder="Search"
           />
         </form>
-        <Link to="/search">or Browse All</Link>
+        <Link to="/search">or Browse All</Link> */}
       </div>
     );
   }
